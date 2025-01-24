a5f79850aec3de9c1654f7a38760afd9b3dae2ed
Alina Bohoslavets <alinabohoslavets@Alinas-MacBook-Pro.local>
2025-01-24 14:51:02 +0400

    first commit

---
+++ src/test/java/com/alina/springmodulithcourse/SpringModulithCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alina.springmodulithcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
